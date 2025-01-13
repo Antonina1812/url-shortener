6d4cf87fefd74fc4ff4f25f28f15f265df531e24
Antonina <146942940+Antonina1812@users.noreply.github.com>
2025-01-13 19:39:18 +0300

    added the necessary go files, main, config, logger, storage, sqlite with connection to the database using sqlite

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	_ "database/sql"
+	_ "fmt"
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug masseges are enabled")
+
+	if _, err := os.Stat("./storage"); os.IsNotExist(err) {
+		err := os.Mkdir("./storage", os.ModeDir|0755)
+		if err != nil {
+			log.Error("failed to create storage directory", sl.Err(err))
+			os.Exit(1)
+		}
+	}
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+	//fmt.Println("Storage initialized:", cfg.StoragePath)
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
